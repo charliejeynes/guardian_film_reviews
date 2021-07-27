995c39a19eac9192c5d987fa52c6d15f66fdb1b3
charliejeynes <31928749+charliejeynes@users.noreply.github.com>
2021-07-27 12:55:04 +0100

    Update guardian_streamlit.py

--- guardian_streamlit.py
+++ guardian_streamlit.py
@@ -2,11 +2,23 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import requests
+import io
+    
+# Downloading the csv file from your GitHub account
+
+url = "https://raw.githubusercontent.com/charliejeynes/guardian_film_reviews/main/data/brads_test.csv" # Make sure the url is the raw version of the file on GitHub
+download = requests.get(url).content
+
+# Reading the downloaded content and turning it into a pandas dataframe
+
+df = pd.read_csv(io.StringIO(download.decode('utf-8')))
+
 st.title('Guardian Film Review Database')
 
 st.write("The below Table is all of Peter Bradshaw's film reviews since 2000-present")
 
-df = pd.read_csv(r'.\data\brads_test.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/charliejeynes/guardian_film_reviews/main/data/brads_test.csv')
 
 df1 = df[['headline', 'webPublicationDate', 'starRating', 'shortUrl', 'bodyText']]
 
